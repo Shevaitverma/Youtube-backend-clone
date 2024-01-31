3830ab39fed6d5ae955754b2a8ac5ba91cbb1619
Shevaitverma <shevaitverma333@gmail.com>
2024-01-31 22:46:08 +0530

    setup and configure app.js and necessary files

--- src/app.js
+++ src/app.js
@@ -9,7 +9,14 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"})) // user limit to receive json files..
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// import routes 
+
+// user routes 
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is running on port: ${process.env.PORT}`);
+        })
+    })
+    .catch((err)=>{
+        console.log("MongoDB connection failed!! ", err);
+    })
