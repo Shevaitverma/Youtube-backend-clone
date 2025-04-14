1373d45d8bce3c3897799e391a3dcee43789e63b
Shevait Verma <54855567+Shevaitverma@users.noreply.github.com>
2025-04-14 13:47:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.use("/", res.send("Working"))
+
 // import routes
 import userRouter from "./routes/user.routes.js";
 
